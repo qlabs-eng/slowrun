99fa37c832a9b721730df248c244544f8a19ce78
not-nonymous <99563940+not-nonymous@users.noreply.github.com>
2026-03-05 14:04:39 -0600

    Update default values for batch size and epochs

--- train.py
+++ train.py
@@ -33,8 +33,8 @@ _script_start = time.time()
 # =============================================================================
 
 parser = argparse.ArgumentParser(description="Train GPT model")
-parser.add_argument("--device-batch-size", type=int, default=4)
-parser.add_argument("--num-epochs", type=int, default=12)
+parser.add_argument("--device-batch-size", type=int, default=8)
+parser.add_argument("--num-epochs", type=int, default=15)
 parser.add_argument("--patience", type=int, default=-1)
 parser.add_argument("--run", type=str, default=None)
 parser.add_argument("--scalar-lr", type=float, default=0.5)
